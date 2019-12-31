0214379f44be5cdf5678837bf3be05706674e3b8
John Koerner <john@johnkoerner.com>
2019-12-30 19:26:42 -0500

    fixing publish steps

--- publish.sh
+++ publish.sh
@@ -4,6 +4,7 @@ then
   echo 'Please commit everything before you publish'
 else
   bundle exec jekyll build
+  git add .
   git commit -m "Build for publish"
   git subtree push --prefix _site origin gh-pages
 fi
